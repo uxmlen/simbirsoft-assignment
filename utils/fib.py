9da33baee4837b8732c31bdff149dd14b5c27a63
uxmlen <38371923+uxmlen@users.noreply.github.com>
2024-05-06 18:56:11 +0300

    fix

--- utils/fib.py
+++ utils/fib.py
@@ -4,12 +4,6 @@ fibonnaci generator
 
 
 def fib(n: int) -> int:
-    if n == 0:
-        return 0
-
-    if n == 1:
-        return 1
-
     a, b = 0, 1
     for _ in range(2, n + 1):
         a, b = b, a + b
